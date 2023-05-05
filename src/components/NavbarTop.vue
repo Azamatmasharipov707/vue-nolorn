e29348470bf74e03f7e552a9e96bce4a8078db08
Azamat <azeraligarx@gmail.com>
2023-05-05 10:57:18 +0500

    nolorn start

---
+++ src/components/NavbarTop.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="navbar-top">
+        <div class="container">
+            <a href="#!" class="navbar-top__logo"><img src="../../public/image/logo.svg" alt=""></a>
+            <div class="navbar-top__input">
+                <select name="" id="">
+                    <option value="">All cotegories</option>
+                    <option value="">All cotegories</option>
+                    <option value="">All cotegories</option>
+                </select>
+                <div>
+                    <input type="search" placeholder="Search...">
+                    <font-awesome-icon :icon="['fasr', 'magnifying-glass']" />
+                </div>
+            </div>
+            <div class="navbar-top__right">
+                <div class="navbar-top__selects">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                    </svg>
+                    <select name="" id="">
+                        <option value="">All cotegories</option>
+                        <option value="">All cotegories</option>
+                        <option value="">All cotegories</option>
+                        <option value="">All cotegories</option>
+                    </select>
+                </div>
+                <div class="navbar-top__shop">
+                    <button>
+                        <span></span>
+                        <font-awesome-icon :icon="['fasl', 'heart']" />
+                        Wishlist
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NavbarTop />
+    </div>
+</template>
+
+<script setup>
+import NavbarTop from '../components/NavbarTop.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
