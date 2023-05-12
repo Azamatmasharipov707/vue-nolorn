a7068b8b53334ce5f48ec696d730c9c1878fef58
Azamat <azer>
2023-05-12 11:01:15 +0500

    cotegories start

---
+++ src/components/Categories.vue
@@ -0,0 +1,87 @@
+<script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel';
+import { reactive } from 'vue';
+
+const shopObj = reactive([
+    {
+        image: '../image/cotegories/Sets.svg',
+        title: 'Sets',
+        bgColor: '#D4EEE2'
+    },
+    {
+        image: '../image/cotegories/fruits.svg',
+        title: 'Fruits',
+        bgColor: '#fff'
+    },
+    {
+        image: '../image/cotegories/Vejetables.svg',
+        title: 'Vejetables',
+        bgColor: '#F4EAEA'
+    },
+    {
+        image: '../image/cotegories/Greenery.svg',
+        title: 'Greenery',
+        bgColor: '#D9F2F9'
+    },
+    {
+        image: '../image/cotegories/Fungi.svg',
+        title: 'Fungi',
+        bgColor: '#F2EBD9'
+    },
+    {
+        image: '../image/cotegories/Groceries.svg',
+        title: 'Groceries',
+        bgColor: '#D9F2F9'
+    },
+    {
+        image: '../image/cotegories/Sweets.svg',
+        title: 'Sweets',
+        bgColor: '#D9F2F9'
+    },
+    {
+        image: '../image/cotegories/Drinks.svg',
+        title: 'Drinks',
+        bgColor: '#D9F2F9'
+    },
+])
+
+/* export default {
+  name: 'App',
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
+} */
+</script>
+
+<template>
+    <section class="cotegories">
+        <div class="container">
+            <div class="cotegories__top">
+                <h2 class="title">shop <span class="title-span">by Categories</span></h2>
+            </div>
+            <carousel :items-to-show="1.5">
+                <slide v-for="slide in 10" :key="slide">
+                    {{ slide }}
+                </slide>
+
+                <template #addons>
+                    <navigation />
+                </template>
+            </carousel>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.cotegories {
+    margin: 40px 0 80px;
+
+    .container {
+        .cotegories__top {}
+    }
+}
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -15,6 +15,32 @@
                         <img  src="../image/header/header-1.png" alt="">
                     </div>
                 </slide>
+                <slide class="header__carousel-slide">
+                    <div class="header__carousel-content">
+                        <h1>Rice drink with vitamin D3</h1>
+                        <p>Up to 30% off your first order</p>
+                        <div class="header__carousel-input">
+                            <input type="text" placeholder="Enter your email address">
+                            <button class="btn">Subscribe</button>
+                        </div>
+                    </div>
+                    <div class="header__carousel-img">
+                        <img  src="../image/header/header-1.png" alt="">
+                    </div>
+                </slide>
+                <slide class="header__carousel-slide">
+                    <div class="header__carousel-content">
+                        <h1>Rice drink with vitamin D3</h1>
+                        <p>Up to 30% off your first order</p>
+                        <div class="header__carousel-input">
+                            <input type="text" placeholder="Enter your email address">
+                            <button class="btn">Subscribe</button>
+                        </div>
+                    </div>
+                    <div class="header__carousel-img">
+                        <img  src="../image/header/header-1.png" alt="">
+                    </div>
+                </slide>
 
                 <template #addons>
                     <!-- <navigation /> -->
@@ -26,6 +52,7 @@
                     Buy with
                     <span>free shipping</span>
                 </h1>
+                <Button />
             </div>
         </div>
     </header>
@@ -34,28 +61,35 @@
 <script>
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import Button from './Button.vue';
 
 export default {
     name: 'App',
     components: {
-        Carousel,
-        Slide,
-        Pagination,
-        Navigation,
-    },
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+    Button
+},
 }
 </script>
 
 <style lang="scss" scoped>
 .header {
+    background: url(../image/header/header-bg.svg) no-repeat #F1FCFF;
+    padding: 30px 0;
     .container {
         display: flex;
+        column-gap: 30px;
         .header__carousel {
             max-width: 770px;
             background: #D9F2F9;
             width: 100%;
-            padding: 46px 31px;
+            padding: 46px 0;
+            
             .header__carousel-slide {
+                padding: 0 31px;
                 .header__carousel-content {
                     font-family: var(--f_Uniform);
                     text-align: start;
@@ -76,8 +110,35 @@ export default {
                         input {
                             width: 100%;
                             padding-left: 10px;
+                            &::placeholder {
+                                color: #b2b2b2;
+                            }
+                        }
+                        button {
+                            width: 106px;
+                            flex-shrink: 0;
                         }
                     }
+                }
+            }
+        }
+        .header__content {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            width: 100%;
+            background: url(../image/header/header-2.png) no-repeat right bottom #F2EBD9;
+            padding-left: 30px;
+            h1 {
+                font-family: var(--f_Uniform);
+                font-weight: 500;
+                font-size: 40px;
+                line-height: 47px;
+                max-width: 168px;
+                width: 100%;
+                margin-bottom: 30px;
+                span {
+                    color: var(--link-hover);
                 }
             }
         }
